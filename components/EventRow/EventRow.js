e63d65f44c8c14551106d13f09fda73838e769d0
Frederico Batista <rincofrederico@gmail.com>
2021-04-23 20:08:27 +0100

    feat: limit by length

--- components/EventRow/EventRow.js
+++ components/EventRow/EventRow.js
@@ -8,45 +8,47 @@ import Grid from "components/Grid/Grid";
 import DateRange from "components/DateRange/DateRange";
 import Link from "next/link";
 
-export const EventRow = ({ event }) => (
+export const EventRow = ({ event, length }) => (
 	<>
 		{groupHasItems(event.data.dates) &&
-			event.data.dates.map((range, key) => (
-				<Grid key={key} subgrid className="mb-2">
-					<Grid.Col sm="span 4" className="h-2 ff-condensed ta-lg-right">
-						<DateRange
-							from={range.dates_from}
-							to={range.dates_to}
-							formats={[
-								(date) => date.format("MMM"),
-								(date) => date.format("DD"),
-							]}
-						/>
-					</Grid.Col>
-					<Grid.Col sm="col-5 / grid-end">
-						<div className="h-5">
-							<Text content={event.data.type || "Espetáculo"} asText />
-						</div>
-						<Link href={hrefResolver(event)}>
-							<a className={styles.link}>
-								<h3 className={`h-2 mb-2`}>
-									<span className="">
-										<Text content={event.data.title} asText />
-									</span>
-								</h3>
-							</a>
-						</Link>
-						<div className="fs-sm">
-							<h4 className="h-3">
-								<Text asText content={range.dates_place} />
-							</h4>
-							<div className="body">
-								<Text content={range.dates_address} />
+			event.data.dates
+				.slice(0, length || event.data.dates.length)
+				.map((range, key) => (
+					<Grid key={key} subgrid className="mb-2">
+						<Grid.Col sm="span 4" className="h-2 ff-condensed ta-lg-right">
+							<DateRange
+								from={range.dates_from}
+								to={range.dates_to}
+								formats={[
+									(date) => date.format("MMM"),
+									(date) => date.format("DD"),
+								]}
+							/>
+						</Grid.Col>
+						<Grid.Col sm="col-5 / grid-end">
+							<div className="h-5">
+								<Text content={event.data.type || "Espetáculo"} asText />
 							</div>
-						</div>
-					</Grid.Col>
-				</Grid>
-			))}
+							<Link href={hrefResolver(event)}>
+								<a className={styles.link}>
+									<h3 className={`h-2 mb-2`}>
+										<span className="">
+											<Text content={event.data.title} asText />
+										</span>
+									</h3>
+								</a>
+							</Link>
+							<div className="fs-sm">
+								<h4 className="h-3">
+									<Text asText content={range.dates_place} />
+								</h4>
+								<div className="body">
+									<Text content={range.dates_address} />
+								</div>
+							</div>
+						</Grid.Col>
+					</Grid>
+				))}
 	</>
 );
 
