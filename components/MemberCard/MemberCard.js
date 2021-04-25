406917a06e241f9e34b2ff3bfb6d344327ba3b55
Frederico Batista <rincofrederico@gmail.com>
2021-04-25 12:34:24 +0100

    feat: if theres no photo show star svg

--- components/MemberCard/MemberCard.js
+++ components/MemberCard/MemberCard.js
@@ -16,16 +16,29 @@ const Wrapper = ({ href, children }) => {
 const MemberCard = ({ name, photo, position, link }) => (
 	<Wrapper href={link}>
 		<figure className={styles.image}>
-			<Placeholder
-				src={photo?.url || "/img/placeholder.png"}
-				width={916}
-				height={567}
-				alt={name}
-				layout="responsive"
-				sizes="(max-width: 768px) 150px,
+			{photo?.url ? (
+				<Placeholder
+					src={photo?.url || "/img/placeholder.png"}
+					width={916}
+					height={567}
+					alt={name}
+					layout="responsive"
+					sizes="(max-width: 768px) 150px,
                                     (max-width: 1920px) 300px,
                                     600px"
-			/>
+				/>
+			) : (
+				<svg
+					xmlns="http://www.w3.org/2000/svg"
+					viewBox="0 0 916 567"
+					fill="currentColor"
+				>
+					<path
+						transform="translate(320,150)"
+						d="M194.2,228.3l-74.4-53.9l-74,53.9L74,141.1L0,87.2h91.7L119.8,0l28.5,87.2H240l-74.4,53.9L194.2,228.3L194.2,228.3z"
+					/>
+				</svg>
+			)}
 		</figure>
 
 		<h3 className="h-3 my-1">
