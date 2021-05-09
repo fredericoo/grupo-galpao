f669f1c6c5f9b6a179131a1c1a925876be540a0a
Frederico Batista <rincofrederico@gmail.com>
2021-05-09 18:53:00 +0100

    fix: stop trying to rich text render objects

--- components/Text/Text.js
+++ components/Text/Text.js
@@ -1,7 +1,7 @@
 import { RichText } from "prismic-reactjs";
 
 const Text = ({ content, asText }) => {
-	if (typeof content === "object") {
+	if (Array.isArray(content)) {
 		if (asText) return RichText.asText(content);
 		return <RichText render={content} />;
 	}
