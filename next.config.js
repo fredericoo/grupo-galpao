12ee9dc15f3c948e91d85b0d446f535225b8fbee
Frederico Batista <rincofrederico@gmail.com>
2021-05-13 20:25:15 +0100

    feat: add instagram cdn

--- next.config.js
+++ next.config.js
@@ -11,6 +11,7 @@ module.exports = {
 			"images.prismic.io",
 			"galpao.cdn.prismic.io",
 			"images.tcdn.com.br",
+			"scontent-iad3-2.cdninstagram.com",
 		],
 		deviceSizes: [320, 640, 750, 828, 1080, 1200, 1920, 2048],
 	},
